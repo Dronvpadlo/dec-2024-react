362b0d3e8bceac28f841b8bb3b17fafccb3621df
User <doronyuk714@mail.com>
2025-04-08 12:12:25 +0300

    lesson 2 complete

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -3,14 +3,16 @@ import {FC} from "react";
 import {IUser} from "../models/IUser.ts";
 
 type MyComponentPropType = {
-    user: IUser
+    user: IUser;
+    vpadlo:(details:IUser) => void
 }
 
-const UserComponent: FC<MyComponentPropType> = ({user}) => {
+const UserComponent: FC<MyComponentPropType> = ({user, vpadlo}) => {
     return(
         <div>{user.id}. {user.name} <br/>
             Mail: {user.email} <br/>
             Phone: {user.phone}
+            <button onClick={() => {vpadlo(user)}}>details</button>
         </div>
     )
 }
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,22 +1,29 @@
 import {useEffect, useState} from "react";
 import {IUser} from "../models/IUser.ts";
 import UserComponent from "./UserComponent.tsx";
+import {getUsers} from "../services/api.service.ts";
 
 const UsersComponent = () => {
 
     const [users, setUsers] = useState<IUser[]>([])
+    const [details, setDetails] = useState<IUser | null>(null)
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
+            getUsers()
             .then(response =>{
                 setUsers(response)
                 console.log("vpadlo")
             })
     }, []);
+
+    const vpadlo = (details: IUser) => {
+        setDetails(details)
+    }
     return (
         <div>
+            <h4>{details?.id}. {details?.name}</h4><br/>
+            <p>Username: {details?.username}. Phone: {details?.phone}. Email: {details?.email}</p>
             {
-                users.map((user, index) => <UserComponent user={user} key={index}/>)
+                users.map((user, index) => <UserComponent vpadlo={vpadlo} user={user} key={index}/>)
             }
         </div>
     );
