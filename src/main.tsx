4abc54cfb50dda45cf3a4a7c1d812dd86a9f612c
User <doronyuk714@mail.com>
2025-04-26 17:45:39 +0300

    init lesson 3 complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import App from "./App.tsx";
+import UsersComponent from "./Components/UsersComponent.tsx";
+import PostsComponent from "./Components/PostsComponent.tsx";
+import CommentsComponent from "./Components/CommentsComponent.tsx";
+import ProductsComponent from "./Components/ProductsComponent.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<App/>}>
+                <Route path={"/users"} element={<UsersComponent/>}/>
+                <Route path={"/posts"} element={<PostsComponent/>}/>
+                <Route path={"/comments"} element={<CommentsComponent/>}/>
+                <Route path={"/products"} element={<ProductsComponent/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
 )
