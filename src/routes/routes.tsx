4f5da265ff4c722d28735d0fc2cb806dbff8c90f
User <doronyuk714@mail.com>
2025-04-28 21:22:36 +0300

    Complete 3.6

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,10 +6,8 @@ import CartsPage from "../pages/CartsPage.tsx";
 
 export const routes = createBrowserRouter([{
     path: '/', element: <MainLayout/>, children:[
-            {
-                path: 'users', element: <UsersPage/>, children: [
-                    {path: ':id/carts', element: <CartsPage/>}
-                ]},
-            {path: 'posts', element: <PostsPage/>}
+            {path: 'users', element: <UsersPage/>},
+            {path: 'posts', element: <PostsPage/>},
+            {path: 'users/:id/carts', element: <CartsPage/>},
         ]},
 ])
