803f7314b6ffb99a9851971c1af01b54f356313b
User <doronyuk714@mail.com>
2025-05-22 21:44:42 +0300

    complete

--- src/components/LeftBranch.tsx
+++ src/components/LeftBranch.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import LeftBranchA from "./LeftBranchA.tsx";
 
 const LeftBranch = () => {
+
+    const memo = useMemo(() => {
+        for (let i = 0; i < 100; i++) {
+            console.log(i)
+
+        }
+        return null
+    }, []);
     return (
         <div>
             Left Branch
